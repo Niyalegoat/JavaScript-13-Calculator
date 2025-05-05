5298d8bfdc238e134fe6e8c5579bd9aa3c108802
Niyalegoat <167811185+Niyalegoat@users.noreply.github.com>
2025-05-05 09:29:25 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+function Solve(val){
+    var v= document.getElementById('res');
+    v.value +=val;
+}
+function Result(){
+    var num1= doucment.getElementById('res').value;
+    try{
+        var num2 = eval(num1.replace('x','*'));
+            document.getElementById('res').value=num2;
+    }catch{
+        document.getElementById('res').value='Error';
+    }
+    }
+function Clear(){
+    var inp =document.getElementById('res');
+    eval.value= ev.value.slice(0,-1);
+}
+document.addEventListener('keydown',function(event){
+    const key = event.key;
+    const validKeys = '0123456789+-*/.%';
+    if(validKeys.includes(key)){
+        Solve(key ==='*'?'x':key);
+    } else if(key==='Backsapce'){
+        Back();
+    }else if(key.toLowerCase()==='c'){
+        clear()
+    }
+});
